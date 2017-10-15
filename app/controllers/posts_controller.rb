992e52e682308b7073dec158afce3913ef3cdbee
Ariel Herman <aeh.herman@gmail.com>
2017-10-15 11:43:13 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,14 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit # the page that renders the edit
+		@post = Post.find(params[:id])
+	end
+
+	def update # talks to the model to update the DB
+		@post = Post.find(params[:id])
+		@post.update(title: params[:title], description: params[:description])
+		redirect_to post_path(@post)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # The above is the same as adding the two routes...
+  # get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  # put 'posts/:id', to: 'posts#update'
 end
